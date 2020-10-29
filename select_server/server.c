c35b26244d36878371af2ae0c5922efcc5a8f31e
YA-LB <67017639+YA-LB@users.noreply.github.com>
2020-10-29 09:18:55 +0800

    Add files via upload

---
+++ select_server/server.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include "wrap.h"
+
+
+#define SERV_PORT 6666
+
+int main(int argc, char *argv[])
+{
+    int i, j, n, maxi;
+
+    int nready, client[FD_SETSIZE];                 
+    int maxfd, listenfd, connfd, sockfd;
+    char buf[BUFSIZ], str[INET_ADDRSTRLEN];        
+
+    struct sockaddr_in clie_addr, serv_addr;
+    socklen_t clie_addr_len;
+    fd_set rset, allset;                            //rset 读事件文件描述符集合 allset用来暂存 
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family= AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port= htons(SERV_PORT);
+
+    Bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    Listen(listenfd, 128);
+    maxfd = listenfd;                                           
+
+    maxi = -1;                                                 
+    for (i = 0; i < FD_SETSIZE; i++)
+        client[i] = -1;                                         
+
+    FD_ZERO(&allset);
+    FD_SET(listenfd, &allset);  // 构造select监控文件描述符集 
+
+    while (1) {   
+        rset = allset;           // 每次循环时都从新设置select监控信号集 
+
+        nready = select(maxfd+1, &rset, NULL, NULL, NULL);  
+        if (nready < 0)
+            perr_exit("select error");
+
+        if (FD_ISSET(listenfd, &rset)) {                        
+
+            clie_addr_len = sizeof(clie_addr);
+            connfd = Accept(listenfd, (struct sockaddr *)&clie_addr, &clie_addr_len);       
+            printf("received from %s at PORT %d\n",
+                    inet_ntop(AF_INET, &clie_addr.sin_addr, str, sizeof(str)),
+                    ntohs(clie_addr.sin_port));
+
+            for (i = 0; i < FD_SETSIZE; i++)
+                if (client[i] < 0) {                            
+                    client[i] = connfd;                         
+                    break;
+                }
+
+            if (i == FD_SETSIZE) {                             
+                fputs("too many clients\n", stderr);
+                exit(1);
+            }
+
+            FD_SET(connfd, &allset);                           
+
+            if (connfd > maxfd)
+                maxfd = connfd;                                 
+
+            if (i > maxi)
+                maxi = i;                                       
+
+            if (--nready == 0)
+                continue;
+        } 
+
+        for (i = 0; i <= maxi; i++) {                               
+
+            if ((sockfd = client[i]) < 0)
+                continue;
+            if (FD_ISSET(sockfd, &rset)) {
+
+                if ((n = Read(sockfd, buf, sizeof(buf))) == 0) 
+				{    
+                    Close(sockfd);
+                    FD_CLR(sockfd, &allset);                      
+                    client[i] = -1;
+                } else if (n > 0) {
+                    for (j = 0; j < n; j++)
+                        buf[j] = toupper(buf[j]);
+                    Write(sockfd, buf, n);
+                    Write(STDOUT_FILENO, buf, n);
+                }
+                if (--nready == 0)
+                    break;                                        
+            }
+        }
+    }
+    Close(listenfd);
+    return 0;
+}
+
